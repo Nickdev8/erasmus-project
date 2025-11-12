1e1312a2bbb14c239b7fdb09b9b26ed986b6a49b
ApollyonLeyssius <38302@ma-web.nl>
2025-11-12 13:15:49 +0100

    settings
    commit

---
+++ Assets/Scripts/main menu/Volumeslider.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Volumeslider : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer; 
+    [SerializeField] private Slider volumeSlider;
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            Loadvolume();
+        }
+        else
+        {
+            setmusicvolume();
+        }
+    }
+
+    public void setmusicvolume()
+    {
+        float volume = volumeSlider.value;
+        audioMixer.SetFloat("music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+
+    private void Loadvolume()
+    { 
+        volumeSlider.value=PlayerPrefs.GetFloat("musicVolume");
+        setmusicvolume();
+
+    }
+}
