e6b8701b9c0ffb1917eac872f6592eb024f1f937
Thanh Tran <156317513+TranThanhThienPhuc@users.noreply.github.com>
2025-11-12 13:20:52 +0100

    projectiles works

--- Assets/Scripts/waffleScript.cs
+++ Assets/Scripts/waffleScript.cs
@@ -5,10 +5,21 @@ public class waffleScript : MonoBehaviour
 {
     [SerializeField] private float waffleSpeed;
     [SerializeField] private Rigidbody2D waffleRB;
+    [SerializeField] private int waffleDamage;
+
 
     // Update is called once per frame
     void Update()
     {
         waffleRB.linearVelocityX += waffleSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<enemyScript>().TakeDamage(waffleDamage);
+            Destroy(gameObject);
+        }
     }
 }
